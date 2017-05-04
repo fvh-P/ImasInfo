81c7fd40a4165c5ce93e3a72285492c82f1320dd
fvh-P <ko_kurihara@yahoo.co.jp>
2017-05-05 01:14:59 +0900

    dotenvのロードを、rbファイルのあるディレクトリに移動した後に変更。

--- ImasInfo.rb
+++ ImasInfo.rb
@@ -4,20 +4,21 @@ require 'mastodon'
 require 'date'
 require 'dotenv'
 
+Dir.chdir(File.expand_path("../", __FILE__))
 Dotenv.load
-f = File.open("#{ENV["LOG_FILE"]}","r")
+f = File.open(File.expand_path("../ImasInfo.log", __FILE__),"r")
 log = f.readlines
 f.close
-f = File.open("#{ENV["LOG_FILE"]}","a")
+f = File.open(File.expand_path("../ImasInfo.log", __FILE__),"a")
 client = Mastodon::REST::Client.new(base_url: ENV["MASTODON_URL"], bearer_token: ENV["MASTODON_ACCESS_TOKEN"])
 src = "http://idolmaster.jp/blog/"
 begin
     doc = Nokogiri::HTML(open(src))
 rescue
+    f.puts()
     f.puts(Time.now)
     f.puts("アクセスできませんでした。\n#{src}")
     f.puts(log[-2])
-    f.puts()
     return
 end
 
