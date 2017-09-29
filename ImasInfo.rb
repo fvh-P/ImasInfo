cc7c6415d23f3c6ffe67026c710093cfc1548ff7
fvh_P <ko_kurihara@yahoo.co.jp>
2017-09-29 11:51:37 +0900

    * correct indent * do nothing if the logfile is incorrect

--- ImasInfo.rb
+++ ImasInfo.rb
@@ -16,36 +16,40 @@ client = Mastodon::REST::Client.new(base_url: ENV["MASTODON_URL"], bearer_token:
 src = "http://idolmaster.jp/blog/"
 
 begin
-    doc = Nokogiri::HTML(open(src))
+  doc = Nokogiri::HTML(open(src))
 rescue
-    f.puts()
-    f.puts(Time.now)
-    f.puts("アクセスできませんでした。\n#{src}")
-    f.puts(log[-2])
-    return
+  f.puts()
+  f.puts(Time.now)
+  f.puts("アクセスできませんでした。\n#{src}")
+  f.puts(log[-2])
+  return
 end
 
 topics = doc.xpath('//div[@id="topics"]').xpath('.//div[@class="inner"]')
 article = topics.css('a')
 post = Array.new
-prevurl = log[-2].chomp
+begin
+  prevurl = log[-2].chomp
+rescue
+  return
+end
 
 article.each_with_index do |e, i|
-    title = e.inner_html
-    url = e.attribute('href').value
-    if(prevurl.eql?(url))
-        break
-    else
-        post << "【アイマス公式ブログ更新情報】\n#{title}\n#{url}\n#imas_blog\n"
-    end
+  title = e.inner_html
+  url = e.attribute('href').value
+  if(prevurl.eql?(url))
+    break
+  else
+    post << "【アイマス公式ブログ更新情報】\n#{title}\n#{url}\n#imas_blog\n"
+  end
 end
 
 post.reverse.each do |e|
-    puts e
-    client.create_status(e)
-    f.puts()
-    f.puts(Time.now)
-    f.puts(e)
+  puts e
+  client.create_status(e)
+  f.puts()
+  f.puts(Time.now)
+  f.puts(e)
 end
 
 f.close
